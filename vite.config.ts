21f039c3ce8bec414c7395ed5c3dbd63750c9856
AerfaKk <1025783794@qq.com>
2026-03-01 20:22:17 +0800

    final: 最终配置，解决GitHub Pages 404

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Vue3-demo/docs/',
+  base: '/Vue3-demo/',
   plugins: [
     vue(),
     vueDevTools(),
